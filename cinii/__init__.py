4c851619ba3e6b16055989fab3b628b8bfb962ed
RTa-technology <scpfoundationidcardmaker@gmail.com>
2022-03-08 11:14:43 +0900

    add: init, requirements.txt

---
+++ cinii/__init__.py
@@ -0,0 +1,15 @@
+"""
+CiNii API wrapper
+This is a wrapper for the CiNii API. It provides a simple interface to
+make requests to the API and return the results as a dictionary.
+The API documentation can be found at
+:copyright: (c) 2022 by the authors and contributors (see AUTHORS).
+:license: MIT, see LICENSE for more details.
+"""
+__title__ = 'cinii'
+__author__ = 'RTa-technology'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2022 RTa-technology'
+__version__ = '0.1.0'
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='cinii',
+  version='0.1.0',
+  license='MIT',
+  description='This is a wrapper for the CiNii API.',
+  author='RTa-technology',
+  packages=["cinii"],
+)
